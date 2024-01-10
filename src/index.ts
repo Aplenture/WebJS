78d9f69b1b49aca76dddc16d8c87594dc08d248f
Aplenture <123380341+Aplenture@users.noreply.github.com>
2024-01-10 11:19:55 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Aplenture/<my_repository_name>
+ * https://github.com/Aplenture/<my_repository_name>
+ * Copyright (c) 2023 Aplenture
+ * License https://github.com/Aplenture/<my_repository_name>/blob/main/LICENSE
+ */
+
